bbfa89d3fdb8100548fc7ce59e82e2b7f2270691
wildanindi2@gmail.com <wildanindi2@gmail.com>
2026-04-21 14:49:13 +0700

    menambahkan UserRepository

---
+++ deploy/src/main/java/com/praktikumDB/deploy/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.praktikumDB.deploy.repository;
+
+import com.praktikumDB.deploy.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
